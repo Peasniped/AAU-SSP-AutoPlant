1ca944152f78454e6922a6c5b02b9a653b215202
Cernott <112928357+Peasniped@users.noreply.github.com>
2023-05-08 13:32:02 +0200

    Update main.py

--- pico_code/main.py
+++ pico_code/main.py
@@ -1,4 +1,22 @@
 # Her sker der ikke noget endnu ):
+import device
+import moisture
+import rotation
 
 if __name__ == "__main__":
-    pass
+    def pseudo_rotate():
+        startup.led.led_rainbow_trail(seconds=5)
+        print("rotation done")
+    
+    startup = device.Startup()
+    print("startup done")
+    moisture = moisture.Moisture()
+
+    while True:
+        # Moisture
+        if moisture.too_dry():
+            moisture.flash()
+
+        #Rotation
+        if rotation.time_to_rotate():
+            pseudo_rotate()
